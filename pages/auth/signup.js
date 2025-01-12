ac09bfb4522b781d11be3fb99e346eb1eada10cd
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-01-12 12:09:53 -0500

    client initialized

---
+++ pages/auth/signup.js
@@ -0,0 +1,9 @@
+const Signup = () =>{
+   return (
+      <form>
+      <h1> Sign up</h1>
+      </form>
+   )
+}
+
+export default Signup;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Landing = () => {
+   return <h1>Landing Page hi</h1>;
+};
+
+export default Landing;
